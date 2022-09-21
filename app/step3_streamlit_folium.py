966349c9d41bb394043e800f2aef2d8a42ac0fd4
amandaaaaaaaaaaaaa <s10243479@connect.np.edu.sg>
2022-09-21 14:48:57 +0800

    4

--- app/step3_streamlit_folium.py
+++ app/step3_streamlit_folium.py
@@ -14,7 +14,7 @@ st.set_page_config(
     menu_items={"About": "Data is updated on one minute interval"})
 
 # create a wallpaper in the app
-set_bg("assets/wall.jpg") 
+set_bg("assets/images.jpg") 
 
 # create headers and information for the app
 head() 
